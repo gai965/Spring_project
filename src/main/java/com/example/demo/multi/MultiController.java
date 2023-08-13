b1f8b7634535fee193a90786b889a737395b2b1f
笠原凱 <s87.kasahara.gai@gmail.com>
2023-08-13 16:39:03 +0900

    create multi

---
+++ src/main/java/com/example/demo/multi/MultiController.java
@@ -0,0 +1,28 @@
+package com.example.demo.multi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.multi.MultiService;
+
+@Controller
+public class MultiController {
+
+	private final MultiService multiService;
+	
+	public MultiController(MultiService multiService) {
+		this.multiService = multiService;
+	}
+	
+	@GetMapping("multi")
+	public String multi(Model model) {
+		String result = "";
+		
+		result = String.valueOf(multiService.multi(7, 7));
+		
+		model.addAttribute("result", result);
+
+		return "multi.html";
+	}
+}
