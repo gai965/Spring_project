c304f7bc7a13559867fc5bcd6c195190adba1bfc
笠原凱 <s87.kasahara.gai@gmail.com>
2023-08-20 00:58:42 +0900

    create controller folder and service folder in calculator folder

--- src/main/java/com/example/demo/calculator/controller/CalculatorController.java
+++ src/main/java/com/example/demo/calculator/controller/CalculatorController.java
@@ -1,9 +1,11 @@
-package com.example.demo.calculator;
+package com.example.demo.calculator.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.calculator.service.CalculatorService;
 
 @Controller
 public class CalculatorController {
