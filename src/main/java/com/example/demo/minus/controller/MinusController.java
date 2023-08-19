a300a0045f8ae834b6d4842fff546a22d424c5d1
笠原凱 <s87.kasahara.gai@gmail.com>
2023-08-20 00:55:00 +0900

    create controller folder and service folder in minus folder

--- src/main/java/com/example/demo/minus/controller/MinusController.java
+++ src/main/java/com/example/demo/minus/controller/MinusController.java
@@ -1,9 +1,11 @@
-package com.example.demo.minus;
+package com.example.demo.minus.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.minus.service.MinusService;
 
 @Controller
 public class MinusController {
